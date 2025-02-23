4fe1559f984de7731e84a68c98a3a457220510db
TuongGiaHan <24021461@vnu.edu.vn>
2025-02-23 22:42:47 +0700

    Create graphics.h

---
+++ T2/graphics.h
@@ -0,0 +1,100 @@
+
+#ifndef _GRAPHICS__H
+#define _GRAPHICS__H
+
+#include <SDL.h>
+#include <SDL_image.h>
+#include "defs.h"
+
+struct Graphics
+{
+    SDL_Renderer *renderer;
+    SDL_Window *window;
+
+void logErrorAndExit(const char* msg, const char* error)
+{
+    SDL_LogMessage(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_ERROR, "%s: %s", msg, error);
+    SDL_Quit();
+}
+
+void init()
+{
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
+        logErrorAndExit("SDL_Init", SDL_GetError());
+
+    if (!IMG_Init(IMG_INIT_PNG | IMG_INIT_JPG))
+        logErrorAndExit( "SDL_image error:", IMG_GetError());
+
+    window = SDL_CreateWindow(WINDOW_TITLE, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    //full screen
+    //window = SDL_CreateWindow(WINDOW_TITLE, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_FULLSCREEN_DESKTOP);
+    if (window == nullptr) logErrorAndExit("CreateWindow", SDL_GetError());
+
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    //Khi chạy trong máy ảo (ví dụ phòng máy ở trường)
+    //renderer = SDL_CreateSoftwareRenderer(SDL_GetWindowSurface(window));
+
+    if (renderer == nullptr) logErrorAndExit("CreateRenderer", SDL_GetError());
+
+    SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "linear");
+    SDL_RenderSetLogicalSize(renderer, SCREEN_WIDTH, SCREEN_HEIGHT);
+
+}
+
+void prepareScene(SDL_Texture * background)
+{
+    SDL_RenderClear(renderer);
+    SDL_RenderCopy( renderer, background, NULL, NULL);
+}
+
+void presentScene()
+{
+    SDL_RenderPresent(renderer);
+}
+
+SDL_Texture *loadTexture(const char *filename)
+{
+    SDL_LogMessage(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_INFO,"Loading %s", filename);
+    SDL_Texture *texture = IMG_LoadTexture(renderer, filename);
+    if (texture == NULL)
+        SDL_LogMessage(SDL_LOG_CATEGORY_APPLICATION,SDL_LOG_PRIORITY_ERROR, "Load texture %s", IMG_GetError());
+
+    return texture;
+}
+
+void quit()
+{
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    IMG_Quit();
+}
+
+void drawSomething(SDL_Window* window, SDL_Renderer* renderer) {
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);   // white
+    SDL_RenderDrawPoint(renderer, SCREEN_WIDTH/2, SCREEN_HEIGHT/2);
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);   // red
+    SDL_RenderDrawLine(renderer, 100, 100, 200, 200);
+    SDL_Rect filled_rect;
+    filled_rect.x = SCREEN_WIDTH - 400;
+    filled_rect.y = SCREEN_HEIGHT - 150;
+    filled_rect.w = 320;
+    filled_rect.h = 100;
+    SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255); // green
+    SDL_RenderFillRect(renderer, &filled_rect);
+}
+
+void renderTexture(SDL_Texture *texture, int x, int y)
+{
+	SDL_Rect dest;
+	dest.x = x;
+	dest.y = y;
+	SDL_QueryTexture(texture, NULL, NULL, &dest.w, &dest.h);
+
+	SDL_RenderCopy(renderer, texture, NULL, &dest);
+}
+
+
+};
+
+#endif
